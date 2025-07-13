6c775a208cdc84d8657cc07121daaef9b4e87ee0
siddhant <siddhant.saxena.2004@gmail.com>
2025-07-13 18:10:06 +0530

    env

--- lib/models/product.model.ts
+++ lib/models/product.model.ts
@@ -1,32 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema(
-    {
-       url:{type:String,required:true,unique:true},
-       currency:{type:String,required:true},
-       image:{type:String,required:true},
-       title:{type:String,required:true},
-       currentPrice:{type:Number,required:true},
-       originalPrice:{type:Number,required:true},
-       priceHistory:[{
-           price:{type:Number,required:true},
-        date:{type:Date,default:Date.now}
-       }],
-       lowestPrice:{type:Number,required:true},
-       highestPrice:{type:Number,required:true},
-       averagePrice:{type:Number,required:true},
-       discountRate:{type:Number,required:true},
-       description:{type:String,required:true},
-       category:{type:String,required:true},
-       reviewsCount:{type:Number,required:true},
-       stars:{type:Number,required:true},
-       isOutOfStock:{type:Boolean,default:false},
-       users:[{
-        email:{type:String,required:true},
-       }],default:[],
-
+const productSchema = new mongoose.Schema({
+  url: { type: String, required: true, unique: true },
+  currency: { type: String, required: true },
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  currentPrice: { type: Number, required: true },
+  originalPrice: { type: Number, required: true },
+  priceHistory: [
+    { 
+      price: { type: Number, required: true },
+      date: { type: Date, default: Date.now }
     },
-    { timestamps: true }
-);
-const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
-export default Product
+  ],
+  lowestPrice: { type: Number },
+  highestPrice: { type: Number },
+  averagePrice: { type: Number },
+  discountRate: { type: Number },
+  description: { type: String },
+  category: { type: String },
+  reviewsCount: { type: Number },
+  isOutOfStock: { type: Boolean, default: false },
+  users: [
+    {email: { type: String, required: true}}
+  ], default: [],
+}, { timestamps: true });
+
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+
+export default Product;
