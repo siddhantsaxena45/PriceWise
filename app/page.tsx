986d4375492e48e9925742600132527bdc63951f
siddhant <siddhant.saxena.2004@gmail.com>
2025-07-13 19:32:10 +0530

    flex

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const Home = async () => {
       </section>
       <section className='trending-section'>
         <h2 className='section-text'>Trending</h2>
-        <div className='flrx flex-wrap gap-x-8 gap-y-16'>{allProducts?.map((product) => (
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>{allProducts?.map((product) => (
           <ProductCard key={product._id} product={product}/>
         ))}</div>
 
